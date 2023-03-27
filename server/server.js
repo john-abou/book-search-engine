f62e11095613c29a4c74e6a9cea39d9d4881fcd5
john-abou <abounassar.john@gmail.com>
2023-03-27 17:16:16 -0400

    Feat: Basic Apollo configuration completed for the server and client.
    Setup the apollo client and wrapped the app component with the apolloProvider.
    Created the apollo server and configured it to run the typedefs/resolvers once defined.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,6 +18,23 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+
+  startApolloServer(typeDefs, resolvers);
